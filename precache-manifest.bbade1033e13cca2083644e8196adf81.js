2a977556f82bad849656c46aca46c4b69495fd4f
chris pat <xristospatakias@gmail.com>
2022-10-20 13:02:22 +0300

    Updates

---
+++ precache-manifest.bbade1033e13cca2083644e8196adf81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "96dd7a8321f0d4df668f",
+    "url": "/robocompany/static/css/main.48eb65e4.chunk.css"
+  },
+  {
+    "revision": "96dd7a8321f0d4df668f",
+    "url": "/robocompany/static/js/main.4d912902.chunk.js"
+  },
+  {
+    "revision": "5ac6323f68a1eff7997b",
+    "url": "/robocompany/static/js/runtime~main.948ea6a6.js"
+  },
+  {
+    "revision": "6f99cff19e39df9657a0",
+    "url": "/robocompany/static/css/2.a722165a.chunk.css"
+  },
+  {
+    "revision": "6f99cff19e39df9657a0",
+    "url": "/robocompany/static/js/2.d8256547.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robocompany/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "56f8e69fd39da2be467679ec4ce1f4cf",
+    "url": "/robocompany/index.html"
+  }
+];
